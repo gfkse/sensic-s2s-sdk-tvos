20d686f26869f336b0854ca625d30a267d8ecaf9
Dimitar Andonov <dimitar.andonov@gfk.com>
2024-10-30 16:19:09 +0100

    A new version has been released

--- Package.swift
+++ Package.swift
@@ -15,18 +15,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "s2s_sdk_tvos_avplayer_googleAds",
-            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.2/s2s_sdk_tvos_avplayer_googleAds.zip",
-            checksum: "602a2011c6f1d16ee5a3d0951d887fe251c8f782c6d5ed6acd622f93e328878f"
+            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.3/s2s_sdk_tvos_avplayer_googleAds.zip",
+            checksum: "d8e9d82d5c7f3d7511392b58761832617699c4a768d831bf4bb07cf0479909bb"
         ),
         .binaryTarget(
             name: "s2s_sdk_tvos_bitmovin",
-            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.2/s2s_sdk_tvos_bitmovin.zip",
-            checksum: "56162d38672ffe7f19bab99a203344d1d80bf5ac2b89ab2e233525d2c8069f6f"
+            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.3/s2s_sdk_tvos_bitmovin.zip",
+            checksum: "0b80e303ee18d410b382a76b0be6c66dd03e9fb2125319dbafd5b06f3eaba3e2"
         ),
         .binaryTarget(
             name: "s2s_sdk_tvos_agent_only",
-            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.2/s2s_sdk_tvos_agent_only.zip",
-            checksum: "3ce26acd70b62fce819ab4790d071097dacb2a4beb65c474fe3d496b561453dd"
+            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.3/s2s_sdk_tvos_agent_only.zip",
+            checksum: "27eaf7451fcab96470aa608f8f5d5aa8a1ca4cbdcd5e9d940c83f423d5bf24e1"
         ),
     ]
 )
