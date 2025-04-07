de01b63162a9e084745aac8c9d97c121f9407d38
Dimitar Andonov <dimitar.andonov@gfk.com>
2025-04-07 10:53:50 +0200

    A new version has been released

--- Package.swift
+++ Package.swift
@@ -8,25 +8,25 @@ let package = Package(
     ],
     products: [
         // Define separate products for each target to keep them isolated
-        .library(name: "AVPlayer-GoogleAds", targets: ["s2s_sdk_tvos_avplayer_googleAds"]),
-        .library(name: "Bitmovin", targets: ["s2s_sdk_tvos_bitmovin"]),
-        .library(name: "AgentOnly", targets: ["s2s_sdk_tvos_agent_only"])
+        .library(name: "AVPlayer-GoogleAds-tvOS", targets: ["s2s_sdk_tvos_avplayer_googleAds"]),
+        .library(name: "Bitmovin-tvOS", targets: ["s2s_sdk_tvos_bitmovin"]),
+        .library(name: "AgentOnly-tvOS", targets: ["s2s_sdk_tvos_agent_only"])
     ],
     targets: [
         .binaryTarget(
             name: "s2s_sdk_tvos_avplayer_googleAds",
-            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.4/s2s_sdk_tvos_avplayer_googleAds.zip",
-            checksum: "8a985f46f8067814dd196afc01db7d9d8d4958852c7df6b5066663fe84c93d44"
+            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.5/s2s_sdk_tvos_avplayer_googleAds.zip",
+            checksum: "fd0afe96a9e1ca0e84a25355efd4327e554f54ceeac981d5bef33ae5ec4eb51f"
         ),
         .binaryTarget(
             name: "s2s_sdk_tvos_bitmovin",
-            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.4/s2s_sdk_tvos_bitmovin.zip",
-            checksum: "669543561b0701939c2bb06e704247ae79a098bbcbd1b3cf52dc5b86ba389896"
+            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.5/s2s_sdk_tvos_bitmovin.zip",
+            checksum: "f62fc66e23c82f42d6abf9d982dccfbece3e1d7755e529a597219b765d5d8a3a"
         ),
         .binaryTarget(
             name: "s2s_sdk_tvos_agent_only",
-            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.4/s2s_sdk_tvos_agent_only.zip",
-            checksum: "1f86d4d1dc2442bf50fc1e0a847d4cc458d2ea3e00549c746d3a5d33be191214"
+            url: "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/1.20.5/s2s_sdk_tvos_agent_only.zip",
+            checksum: "b53c4ae8ee29c7011e35003313cb7557a651fb39eda7ada56b38dd86db9f8734"
         ),
     ]
 )
